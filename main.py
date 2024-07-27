3918673752c6c99f4b040c5f333c11802f5b733a
glitteryHippo <glitteryhippo2600@gmail.com>
2024-07-27 05:58:09 +0000

    Added scatter splot

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import os
-from flask import Flask, request, send_from_directory, jsonify, render_template
+from flask import Flask, request, send_from_directory, jsonify, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
-
 import xmltodict
 import pandas as pd
 import numpy as np
-
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import numpy as np
@@ -41,30 +39,28 @@ def upload_file():
       filename = secure_filename(file.filename)
       file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
       send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-      return jsonify('File uploaded successfully')
+      return redirect(url_for("output", filename=filename, data_type=text))
   types = open("types.txt").readlines()
   return render_template(os.path.join('upload.html'), types=types)
   
-def parse_file(filename):
-  with open(os.path.join(UPLOAD_FOLDER, filename)) as file:
+def parse_file(filename, data_type):
+  with open(os.path.join(app.config['UPLOAD_FOLDER'], filename)) as file:
     data_dict = xmltodict.parse(file.read())
 
   health_data = data_dict['HealthData']['Record']
   df = pd.DataFrame(health_data)
 
-  df['@creationDate'] = pd.to_datetime(df['@creationDate'])
+  '''df['@creationDate'] = pd.to_datetime(df['@creationDate'])
   df['@startDate'] = pd.to_datetime(df['@startDate'])
-  df['@endDate'] = pd.to_datetime(df['@endDate'])
+  df['@endDate'] = pd.to_datetime(df['@endDate'])'''
 
   df['@type'] = df['@type'].str.replace('HKQuantityTypeIdentifier', '')
 
-  print(df)
-  return df
-
-@app.route('/analyze/<filename>/<data_type>', methods=['GET','POST'])
-def analyze(filename, data_type):
-  df = parse_file(filename)
   data_type_records = df[df['@type'] == data_type]
+
+  return data_type_records
+
+def analyze(data_type_records):
 
   if data_type_records.empty:
     return jsonify('No records found for the specified data type'), 400
@@ -79,16 +75,9 @@ def analyze(filename, data_type):
     'std': np.std(values)
   }
 
-  return jsonify(summary)
+  return summary
 
-@app.route('/predict/<filename>/<data_type>', methods=['GET'])
-def predict(filename, data_type):
-  df = parse_file(filename)  
-
-  data_type_records = df[df['@type'] == data_type]
-
-  if data_type_records.empty:
-    return jsonify('No records found for the specified data type'), 400
+def predict(data_type_records):
 
   values = data_type_records['@value'].astype(float)[:-1].values.reshape(-1,1)
   previous_values = data_type_records['@value'].astype(float)[1:].values.reshape(-1,1)
@@ -102,7 +91,32 @@ def predict(filename, data_type):
   y_pred = model.predict(X_test)
   accuracy = model.score(X_test, y_test)
 
-  return jsonify({"accuracy": accuracy, "predictions": y_pred.tolist()})
+  return {"accuracy": accuracy, "predictions": y_pred.tolist()}
+
+@app.route('/output/<filename>/<data_type>', methods=['GET'])
+def output(filename, data_type):
+  data_type_records = parse_file(filename, data_type)
   
+  if data_type_records.empty:
+    return jsonify('No records found for the specified data type'), 400
+    
+  analytics = analyze(data_type_records)
+  prediction = predict(data_type_records)
+
+  output = prediction
+  output['analysis'] = analytics
+
+  flatList = []
+  
+  for element in prediction['predictions']:
+    if type(element) is list:
+        # Check if type is list than iterate through the sublist
+        for item in element:
+            flatList.append(item)
+    else:
+        flatList.append(element)
+
+  return render_template('graph.html',labels=data_type_records['@creationDate'].tolist(), values1=data_type_records['@value'].astype(float).tolist(), values2=flatList)
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000)
